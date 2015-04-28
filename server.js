474b4b3d5474a965acba7cc39c3b203ca0fe6152
Rick Murt <rickmurt@gmail.com>
2015-04-27 22:34:44 -0700

    Setting up basic querying of omdbapi.  Need to find out if I'm allowed to use a rendering engine like EJS

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,6 +28,17 @@ app.post('/favorites', function(req, res){
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
+});
+
+app.get('/search', function(req, res){
+  var query = req.param('q');
+
+  request('http://www.omdbapi.com/?s=' + query, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      res.send(JSON.parse(body));
+    }
+  });
+
 });
 
 app.listen(3000, function(){
