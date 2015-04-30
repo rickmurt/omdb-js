a4bb0f14054971b9fd3e998caa676e8b4ad3f636
Rick Murt <rickmurt@gmail.com>
2015-04-29 17:32:48 -0700

    Refactor app quite a bit to use innerHTML instead of createElement as it's much faster and more readable (in my opinion).  Add lots more comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,72 @@
 (function(){
+  // we'll use this to control the logic flow
+  document.querySelector('form').addEventListener('submit', function(e){
+    // prevent the form from actually submitting to the server,
+    // we'll handle what happens via javascript instead
+    e.preventDefault();
 
-  // convenience method to prevent typing document.querySelector every time
-  var $ = function(selector){
-    return document.querySelector(selector);
+    // get the value of the search input
+    var input = document.querySelector('#q').value;
+
+    // create object to make ajax request
+    var req = new XMLHttpRequest();
+
+    // specify the URL of the 3rd party API, encoding the search query properly
+    req.open('GET', 'http://omdbapi.com/?s=' + encodeURIComponent(input));
+
+    // check for errors when trying to contact the 3rd party API
+    // this is different from an error returned by the API (like movie not found).
+    // this would occur if the API were down, for example
+    req.addEventListener('error', function(res){
+      updateSearchResults('Error reaching OMDb API');
+    });
+
+    // define a callback function to fire once the request has been made
+    req.addEventListener('load', function(res){
+      // parse the response as JSON so we can easily manipulate with javascript
+      var response = JSON.parse(res.target.response);
+
+      // if there is an error, display it and stop processing
+      // this would be an error actually returned by the API
+      if (response.Error) {
+        updateSearchResults(response.Error);
+        return false;
+      }
+
+      // the API response is wrapped in "Search", we're only concerned about that
+      var html = generateResultsHtml(response.Search);
+
+      // update the search results with the generated html.  we just take an array
+      // and concatenate it all together as a string with no spaces or characters in between.
+      updateSearchResults(html.join(''));
+
+      // register add to favorites click event to all add to favorites buttons
+      var favoritesButtons = document.querySelectorAll('.js-add-to-favorites');
+      for (var i = 0; i < favoritesButtons.length; i++){
+        var button = favoritesButtons[i];
+        button.addEventListener('click', favoriteClickEvent);
+      }
+
+      // register fetch API info click event to all headers
+      var headers = document.querySelectorAll('.js-fetch-imdb-info');
+      for (var i = 0; i < headers.length; i++){
+        var header = headers[i];
+        header.addEventListener('click', fetchMovieBodyClickEvent);
+      }
+    });
+
+    // finally, send the request
+    req.send();
+  });
+
+  function updateSearchResults(html){
+    document.querySelector('#search-results').innerHTML = html;
   }
 
   // event handler to attach to dynamically created panels.
   // the panels are created when a search occurs and returns information like title and ID,
   // we'll then re-use that ID to query for more information in this click function
-  function movieBodyClickEvent() {
+  function fetchMovieBodyClickEvent(e){
     var children = this.children;
 
     // this should be present from the search request
@@ -16,7 +74,10 @@
 
     // loop through children to find the class we're looking for
     for(var i = 0; i < children.length; i++){
-      if (children[i].className != "panel-body"){
+      // if this child is anything other than panel-body, we don't care about it and can continue
+      // since there could be multiple classes, we check to see if the className string contains
+      // the class that we're looking for
+      if (children[i].className.indexOf("panel-body") == -1){
         continue;
       }
 
@@ -25,10 +86,20 @@
       if (children[i].innerText != "") {
         break;
       }
+
+      // make a new request
       var req = new XMLHttpRequest();
 
       // specify the URL of the 3rd party API, encoding the search query properly
       req.open('GET', 'http://omdbapi.com/?i=' + encodeURIComponent(oid));
+
+      // check for errors when trying to contact the 3rd party API
+      // this is different from an error returned by the API (like movie not found).
+      // this would occur if the API were down, for example
+      req.addEventListener('error', function(res){
+        children[i].innerHTML = 'Error reaching OMDb API';
+      });
+
       req.addEventListener('load', function(res){
 
         // parse the response as JSON so we can easily manipulate with javascript
@@ -37,10 +108,13 @@
         // create the inner HTML using some bootstrap classes
         var html = [];
         html.push('<ul class="list-group">');
+        // we'll just push all of the data we're returned from the API
+        // separating keys (like "Year" or "Genre") with values (like the actual year or name of genre)
         for (var key in result) {
           html.push('<li class="list-group-item"><strong>' + key + '</strong>: ' + result[key] + '</li>');
         }
         html.push('</ul>');
+        // we just take an array and concatenate it all together as a string with no spaces or characters in between.
         children[i].innerHTML = html.join('');
       });
       req.send();
@@ -52,102 +126,57 @@
   // event handler to attach to dynamically created favorite buttons.
   // the buttons are created when a search occurs and has a payload as a data attribute
   // that we can send to the back end to save to favorites
-  function favoriteClickEvent() {
+  function favoriteClickEvent(e){
+    // make a new request, and send the data-favorite object to the backend API to save
+    // to favorites
     var favoritePayload = this.getAttribute('data-favorite');
     var req = new XMLHttpRequest();
     req.open('POST', 'http://localhost:3000/favorites');
+    // tell the server we're sending application/json (remember we set that in server.js with bodyParser.json())
     req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     req.send(favoritePayload);
     // add the filled in star icon to replace the empty star icon to indicate state has changed
     this.children[0].className = "glyphicon glyphicon-star";
   }
 
-  $('form').addEventListener('submit', function(e){
-    // prevent the form from actually submitting to the server,
-    // we'll handle what happens via javascript instead
-    e.preventDefault();
+  // created this as a separate function because the search results can be updated
+  // in a bunch of different places, and those places shouldn't have to be aware
+  // of the query selector to complete the update
+  function updateSearchResults(html){
+    // using innerHTML rather than createElement because it's much faster
+    // https://jsperf.com/innerhtml-vs-createelement-test/30
+    document.querySelector('#search-results').innerHTML = html;
+  }
 
-    // clear any results that may already be there for a new search
-    $('#search-results').innerHTML = '';
+  // loop through, building html for our search-results div
+  // created this as a separate function because this function
+  // shouldn't be concerned about the implementation of how it got the results (the xhr below)
+  // only that it has some results, and that it'll have some data
+  function generateResultsHtml(results){
+    var html = [];
+    for(var i = 0; i < results.length; i++){
+      // create a javascript object that can be passed to our save to favorites API
+      var favoritePayload = {};
+      favoritePayload.oid = results[i].imdbID;
+      favoritePayload.name = results[i].Title;
 
-    // get the value of the search input
-    var input = $('#q').value;
+      // create a search result container with a heading and body. we should save the imdbID to the header
+      // so that when we click on it, we can easily use that to query the API later
+      html.push('<div class="panel panel-default js-fetch-imdb-info" data-oid="' + results[i].imdbID + '">');
+        // heading should contain a button to add to favorites, and the title of the movie
+        html.push('<div class="panel-heading">');
+          // notice the escaped quote for data-favorite, as proper JSON always has double quotes
+          html.push('<button type="button" class="btn btn-default js-add-to-favorites" data-favorite=\'' + JSON.stringify(favoritePayload) + '\'>');
+            html.push('<span class="glyphicon glyphicon-star-empty" title="Add to favorites"></span>');
+          html.push('</button>');
+          html.push('<span>' + results[i].Title + '</span>');
+        html.push('</div>');
+        html.push('<div class="panel-body"></div>');
+      html.push('</div>');
+    }
+    return html;
+  }
 
-    // create object to make ajax request
-    var req = new XMLHttpRequest();
-
-    // specify the URL of the 3rd party API, encoding the search query properly
-    req.open('GET', 'http://omdbapi.com/?s=' + encodeURIComponent(input));
-
-    // define a callback function to fire once the request has been made
-    req.addEventListener('load', function(res){
-      console.log(res);
-
-      // parse the response as JSON so we can easily manipulate with javascript
-      var response = JSON.parse(res.target.response);
-
-      // if there is an error, display it and stop processing
-      if (response.Error) {
-        $('#search-results').innerHTML = response.Error;
-        return false;
-      }
-
-      results = response.Search;
-
-      // loop through, outputting all of the results
-      for(var i = 0; i < results.length; i++){
-        // create a the result container
-        var result = document.createElement('div');
-
-        // apply some bootstrap classes
-        result.className = "panel panel-default js-fetch-imdb-info";
-
-        // save the imdbID to query for more info if a user clicks
-        result.setAttribute('data-oid', results[i].imdbID);
-        // register click event
-        result.onclick = movieBodyClickEvent;
-
-        // create the heading
-        var heading = document.createElement('div');
-        heading.className = "panel-heading";
-
-        // create a javascript object that can be passed to our save to favorites API
-        var favoritePayload = {};
-        favoritePayload.oid = results[i].imdbID;
-        favoritePayload.name = results[i].Title;
-
-        // create the favorite button
-        var favorite = document.createElement('button');
-        favorite.setAttribute('type', 'button');
-        favorite.className = "btn btn-default js-add-to-favorites";
-        favorite.setAttribute('data-favorite', JSON.stringify(favoritePayload));
-        favorite.onclick = favoriteClickEvent;
-
-        // create the star glyphicon and append it to the favorite button
-        var star = document.createElement('span');
-        star.className = "glyphicon glyphicon-star-empty";
-        star.title = "Add to favorites";
-        favorite.appendChild(star);
-
-        // create title span and append it to heading
-        heading.appendChild(favorite);
-        var title = document.createElement('span');
-        title.innerText = results[i].Title;
-        heading.appendChild(title);
-
-        // create the body
-        var body = document.createElement('div');
-        body.className = "panel-body";
-
-        // add the heading and body as children of the container
-        result.appendChild(heading);
-        result.appendChild(body);
-
-        // add to search results
-        $('#search-results').appendChild(result);
-      }
-    });
-    req.send();
-  });
-
+// wrapping all functionality into an IIFE (Immediately-Invoked Function Expression) to not allow these functions
+// to leak outside of this code block
 })();
--- server.js
+++ server.js
@@ -1,46 +1,49 @@
+// require all the items we need.  these are included with node,
+// or they're defined as dependencies in package.json
 var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
-// var request = require('request');
 
+// tell the app to use /public as the root directory for serving HTML
 app.use(express.static(path.join(__dirname, '/public')));
+// setting extended to false uses the querystring library instead of the qs library
+// meaning, application/x-www-form-urlencoded parser
 app.use(bodyParser.urlencoded({ extended: false }));
+// allow us to parse application/json
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
-
+// when the server tries to access the URL at /favorites...
 app.get('/favorites', function(req, res){
+  // open a file, read the data (which will be JSON)...
   var data = fs.readFileSync('./data.json');
+  // say we're sending application/json..
   res.setHeader('Content-Type', 'application/json');
+  // and send it!
   res.send(data);
 });
 
+// when the server tries to POST to /favorites...
 app.post('/favorites', function(req, res){
+  // require that the POST body contains two keys: name and oid
   if(!req.body.name || !req.body.oid){
+    // if it doesn't, send an error
     res.send("Error");
     return;
   }
 
+  // get the data currently in the file
   var data = JSON.parse(fs.readFileSync('./data.json'));
+  // add the data POSTed from the request body to the existing data
   data.push(req.body);
+  // rewrite the file with the data appended
   fs.writeFile('./data.json', JSON.stringify(data));
   res.setHeader('Content-Type', 'application/json');
   res.send(data);
 });
 
-// app.get('/search', function(req, res){
-//   var query = req.param('q');
-
-//   request('http://www.omdbapi.com/?s=' + query, function(error, response, body){
-//     if (!error && response.statusCode == 200) {
-//       res.send(JSON.parse(body));
-//     }
-//   });
-
-// });
-
+// tell the server to listen to port 3000
 app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
